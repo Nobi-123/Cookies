78131c8cd10a3a7a121610353bc8a8868dc4b3d5
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-26 18:16:51 +0530

    Update __main__.py

--- Modules/__main__.py
+++ Modules/__main__.py
@@ -3,7 +3,7 @@ import os
 from pyrogram import Client
 
 API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 
 # The app is already created in start.py, so we just run it
