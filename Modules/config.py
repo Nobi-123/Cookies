1bc72fa40d4bf0223d6d8a316c8cee198714ee57
Nobi-123 <nobita.infringement.report@gmail.com>
2025-09-26 16:33:19 +0530

    Update config.py

--- Modules/config.py
+++ Modules/config.py
@@ -9,7 +9,7 @@ API_HASH = os.getenv("API_HASH", "your_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 DEFAULT_TIMEZONE = os.getenv("TIMEZONE", "Asia/Kolkata")
 DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "yes")
-REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL", "TMCnetwork")
+REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL", "TNCnetwork")
 LOG_CHANNEL = os.getenv("LOG_CHANNEL", "-1003065367480")
 from Modules.utils.default_cookies import get_default_cookie
 DEFAULT_COOKIE = get_default_cookie()
